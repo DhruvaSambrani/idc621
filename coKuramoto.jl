7edd1c2a086d12c97b37d8dfbbdfc8b3d7aa53a4
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2021-03-13 23:25:42 +0530

    make mutable

--- coKuramoto.jl
+++ coKuramoto.jl
@@ -1,38 +1,46 @@
+println("Starting script with $(Threads.nthreads())")
 using Distributions
 using Statistics
+using BenchmarkTools
 using Plots
 
 println("imported")
+
 #%%
-struct Agent
+mutable struct Agent
     R::Float64
     V::Float64
 end
 
 init(n, σ) = Agent.(1 + 0im, rand(Normal(0., σ), n))
 Statistics.mean(arr::Array{Agent}) = mean(cis.(getproperty.(arr, :R)))
-function update(agent::Agent, meanvec, κ, dt) 
-    Agent(
-        agent.R + (
-                agent.V + 
-                κ * abs(meanvec) * sin(angle(meanvec) - agent.R)
-            ) * dt,
-        agent.V
-    )
+function update!(agent::Agent, meanvec, κ, dt)
+    agent.R += (agent.V + κ * abs(meanvec) * sin(angle(meanvec) - agent.R)) * dt
 end
-function evolve(agents::Array{Agent}, κ, dt)
+function evolve!(agents::Array{Agent}, κ::Float64, dt::Float64)
     meanvec = mean(agents)
-    meanvec, update.(agents, meanvec, κ, dt)
+    Threads.@threads for i in eachindex(agents)
+        update!(agents[i], meanvec, κ, dt)
+    end
+    meanvec
 end
 function simulate(steps::Int, n::Int, σ::Float64, κ::Float64, dt::Float64)
     agents = init(n, σ)
     means = Array{Complex,1}(undef, Int(steps))
-    records = Array{Complex,1}(undef, Int(steps))
     for step in 1:steps
-        means[step], agents = evolve(agents, κ, dt)
+        means[step] = evolve!(agents, κ, dt)
     end
     means
 end
+
+
+#%%
+
+# @show @benchmark evolve(init(500, 0.3), 0.5, 1e-3)
+
+#%%
+
+
 function graph(κs, σs)
     arr = zeros(Float64, length(κs), length(σs))
     for (i, κ) in enumerate(κs), (j, σ) in enumerate(σs)
@@ -42,11 +50,12 @@ function graph(κs, σs)
     arr
 end
 # %%
-κs, σs = 0:0.2:2, 0:0.1:1
+
+κs, σs = 0:0.1:2, 0:0.1:2
 zs = graph(κs, σs)
 # %%
-plotlyjs()
 plot(σs, κs, zs, st=:surface, zlims=(0, 1), xlabel="Sigma", ylabel="Kappa", zlabel="r_inf", title="R∞(σ, κ)")
 savefig("ksr")
+
 # %%
-plot(abs.(simulate(Int(1e5), 500, 1., 0., 1e-4)))
+# plot(abs.(simulate(Int(1e5), 500, 1., 0., 1e-4)))
